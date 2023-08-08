8878c219bfa41314b32f239313c7f8d6b5b0c5c6
AdautoEC <adauto.ec@gmail.com>
2023-08-08 13:27:25 -0300

    Initial commit

---
+++ app/src/main/java/com/k4tr1n4/marvel/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.k4tr1n4.marvel
+
+import android.os.Bundle
+import com.k4tr1n4.marvel.databinding.ActivityMainBinding
+import com.skydoves.bindables.BindingActivity
+
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding {
+
+        }
+    }
+}
